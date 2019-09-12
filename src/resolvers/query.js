919a8bda430adf548e1e972ddb33c12edee37cf1
Adrian Videnie <avidenie@gmail.com>
2019-09-12 20:50:03 +0300

    feat: set up Apollo server

---
+++ src/resolvers/query.js
@@ -0,0 +1,25 @@
+function info() {
+  return `This is the API of Expect TV`
+}
+
+function movies() {
+  return [
+    {
+      id: 474350,
+      title: 'It Chapter Two'
+    },
+    {
+      id: 384018,
+      title: 'Fast & Furious Presents: Hobbs & Shaw'
+    },
+    {
+      id: 420818,
+      title: 'The Lion King'
+    }
+  ]
+}
+
+module.exports = {
+  info,
+  movies
+}
