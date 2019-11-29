5702205c5b1c8801358b81ff7ee63798ec02f851
Adrian Videnie <avidenie@gmail.com>
2019-11-29 17:48:34 +0200

    Add pagination for popular queries

--- src/schema.js
+++ src/schema.js
@@ -2,8 +2,16 @@ const { gql } = require('apollo-server')
 
 const schema = gql`
   type Query {
-    popularMovies(region: String = "US", language: String = "en"): [Movie!]!
-    popularTvShows(region: String = "US", language: String = "en"): [TvShow!]!
+    popularMovies(
+      region: String = "US"
+      language: String = "en"
+      page: Int = 1
+    ): MovieResults!
+    popularTvShows(
+      region: String = "US"
+      language: String = "en"
+      page: Int = 1
+    ): TvShowResults!
   }
 
   type Movie {
@@ -23,6 +31,22 @@ const schema = gql`
     thumbnail: String
     logo: String
     backgrounds(limit: Int = 1): [String]!
+  }
+
+  type MovieResults {
+    results: [Movie!]!
+    pageInfo: PageInfo!
+  }
+
+  type TvShowResults {
+    results: [TvShow!]!
+    pageInfo: PageInfo!
+  }
+
+  type PageInfo {
+    page: Int!
+    totalPages: Int!
+    totalResults: Int!
   }
 `
 
