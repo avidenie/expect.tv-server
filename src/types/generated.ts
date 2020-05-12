fbb3553e8b266835bb92578f984cbf928067c010
Adrian Videnie <adrian.videnie@softvision.ro>
2020-05-12 23:25:01 +0300

    Update outdated dependencies

--- src/types/generated.ts
+++ src/types/generated.ts
@@ -813,5 +813,5 @@ export type Resolvers<ContextType = any> = ResolversObject<{
 /**
  * @deprecated
  * Use "Resolvers" root object instead. If you wish to get "IResolvers", add "typesPrefix: I" to your config.
-*/
+ */
 export type IResolvers<ContextType = any> = Resolvers<ContextType>;
